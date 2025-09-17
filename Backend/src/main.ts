efc2e94d53972cf06e775eea573842b492637d30
Kunal Patil <138603748+kunal0395@users.noreply.github.com>
2025-09-17 23:14:13 +0530

    Update CORS main.ts

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173'], 
+    origin: ['https://schoolapplicationass.netlify.app'], 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
